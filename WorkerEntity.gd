2740e18545db2727336106dfae94722e931bd8ac
solalbcn <45416694+solalbcn@users.noreply.github.com>
2024-09-18 17:13:22 +0200

    worker entity

---
+++ WorkerEntity.gd
@@ -0,0 +1,12 @@
+extends StaticBody2D
+
+var sprite 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Sprite2D.region_rect=Rect2(408+17*randi_range(0,7),17*randi_range(0,4),16,16)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
